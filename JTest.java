d9995ff46e6b54a05fb027e39e35bf34cc02f650
nayakrahul2409 <nayak.rahul2409@gmail.com>
2019-02-04 15:48:55 +0530

    Add files via upload

---
+++ JTest.java
@@ -0,0 +1,14 @@
+package com.psl.junit;
+
+public class JTest {
+	public static void main(String[] args) {
+		int x,y,z;
+		x=5;
+		y=10;
+		z=add(x,y);
+	}
+    static int add(int x,int y)
+    {
+    	return (x+y);
+    }
+}
